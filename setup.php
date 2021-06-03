198cceb2b60523a557ddafc80aab81731ca0a4f0
Claudia Peart <peart.claudia@gmail.com>
2021-06-03 16:52:03 -0400

    added setup.php
    added setup.php

---
+++ setup.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+All codes for the Exponential Profit Multiplier- 
+Copyright (C) 2006 by Jahn A. SupernovaFiresale3.com 
+All rights reserved. Unauthorised copying, re-distributing, 
+re-selling are strictly prohibited and punishable by law.
+
+INTRODUCTION FOR PHP NEWBIES:
+Follow the instructional comments as closely as possible. 
+Your configurations are unique to you only. That which is 
+already entered here are just examples. Your unique 
+configurations belong ONLY within the "quotation marks, 
+like this"; and NOT OUTSIDE them -> "".
+
+NOTE: If you're viewing this in Notepad, TURN OFF WORD WRAP.
+And ONLY view this in Notepad or any web editing software 
+you have that supports PHP, like Dreamweaver.
+*/
+
+// Your Name.
+$myname = "Jahn";
+
+// Your Email address.
+$myemail = "jahn@noorazan.com";
+
+// The full URL of the website you are promoting through the VMA.
+// Look at the example syntax below.
+// BEGIN with http:// END with a / Slash.
+$mywebsite = "http://www.mockupsite.com/";
+
+// The directory in your website where you place all your 
+// Viral Marketing Accelerator files. Default:- "vma/" No need 
+// to change. ENDS WITH A / SLASH:-
+$myvma = "vminstaller/2/";
+
+// Just the domain and the extension.
+// NO www. NO / slash NO http://
+$mydomain = "www.mockupsite.com";
+
+// The full URL of the page that you want to bring your
+// referrers to after they've entered the names & emails
+// of their 2 friends and submitted the form.
+$thankyou = "http://www.articlerecon.com/lite/";
+
+// Full URL to your affiliate signup page
+$affiliatesignup = "http://www.favcart.com/upgrade";
+
+// What the affiliate link will look like.
+// E.g. Clickbank affiliate link format:-
+// http://hop.clickbank.net/?AFFILIATE-ID/MERCHANT-ID
+// Of course, you can use ANY affiliate program system,
+// not just ClickBank. Take whatever link format your
+// affiliate program system uses and replace the characters
+// that are meant for the AFFILIATE ID with ' $v ', like this:-
+$myaffiliateurl = "http://www.supernovafiresale3.com/snvf3.html?aff_id=$v";
+
+// Subject of the Email that will be sent out
+// to the Referred Friends of your visitors.
+// Keep it short, simple & commanding.
+$subject = "You are not going to believe this!";
+
+// The Email message that is to be sent out
+// to the Referred Friends of your visitors.
+// Make it short & friendly. Avoid excessively
+// promotional-sounding language. Yet make it
+// sound beneficial. Appropriately SPAM-FILTER-PROOF
+// your message where required.
+// NO NEED to add "Hi" or "Dear Friend" etc as these
+// will be automatically added for you.
+// Note: If you need to use " quotation " marks,
+// add a \ slash before the mark, like this:- \"quote\"
+// You don't have to add the \ slash for single ' quotes '.
+$message = "
+I came across this thing and thought I\'d be too guilty not 
+
+to let you see it too, knowing that you\'re passionate about 
+
+internet marketing, just like me.
+
+
+
+It\'s quite simple, really. Can you answer these 3 questions?
+";
+
+// You don't have to put the Referral Link after "Click here ->"
+// in the message text above. It will be automatically added for you.
+
+// The Email message that is to be sent out
+// to your Affiliates or Referrers.
+// Note: If you need to use " quotation " marks,
+// add a \ slash before the mark, like this:- \"quote\"
+// You don't have to add the \ slash for single ' quotes '.
+// Appropriately change 'Exponential Profit Multiplier' to 
+// whatever the name of your product or website is.
+// Appropriately change '75% perpetual commissions' to 
+// whatever your intended commission offer is.
+// NO NEED to add "Hi" or "Dear Friend" etc as these
+// will be automatically added for you.
+$thankumessage = "
+Thanks for referring your 2 friends to the (REPLACE THIS WITH YOUR PRODUCT NAME.)
+
+
+
+Here\'s the referral link you can use to promote the (REPLACE THIS WITH YOUR PRODUCT NAME.) and earn X% perpetual commissions
+";
+// You don't have to put the Referral Link after "Click here ->"
+// in the message text above. It will be automatically added for you.
+
+
+// Insert the amount that you intend to sell your package at
+$amount = "55";
+
+// Insert the amount of your product after discount
+$amountdisc = "35";
+
+
+// What Is the name of your product?
+$productname = "Exponential Profit Multiplier";
+
+
+// Insert the commision amount for the product
+$commissionamount = "50%";
+
+
+?>
